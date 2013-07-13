e0e4e9d40143961c82ceb0f68e9d1db02b83b9d5
Jacob Swanner <jacob@jacobswanner.com>
2013-07-13 17:43:52 -0400

    Extract trigonometry functions into own gem

--- lib/arel/nodes/haversine.rb
+++ lib/arel/nodes/haversine.rb
@@ -1,10 +1,12 @@
+require 'arel/trigonometry'
+
 module Arel
   module Nodes
     class Haversine < Function
       def initialize lat1, lng1, lat2, lng2, options = nil
         super Multiplication.new(
           Arcsine.new(
-            Sqrt.new(
+            SquareRoot.new(
               Addition.new(
                 Pow.new(Sine.new(Division.new(Radians.new(lat1 - lat2), 2)), 2),
                 Multiplication.new(
@@ -26,56 +28,9 @@ module Arel
         (options || {})[:unit] == :mi ? 7917.5 : 12742
       end
     end
-
-    %w[
-      Arcsine
-      Cosine
-      Radians
-      Sine
-      Sqrt
-    ].each do |name|
-      const_set name, Class.new(Unary)
-    end
-    Pow = Class.new(Binary)
   end
 
   module Visitors
-    module CommonMathVisitors
-      private
-
-      def visit_Arel_Nodes_Arcsine o
-        "ASIN(#{visit o.expr})"
-      end
-
-      def visit_Arel_Nodes_Cosine o
-        "COS(#{visit o.expr})"
-      end
-
-      def visit_Arel_Nodes_Pow o
-        "POW(#{visit o.left}, #{visit o.right})"
-      end
-
-      def visit_Arel_Nodes_Radians o
-        "RADIANS(#{visit o.expr})"
-      end
-
-      def visit_Arel_Nodes_Sine o
-        "SIN(#{visit o.expr})"
-      end
-
-      def visit_Arel_Nodes_Sqrt o
-        "SQRT(#{visit o.expr})"
-      end
-    end
-
-    class MySQL
-      include CommonMathVisitors
-    end
-
-    class PostgreSQL
-      include CommonMathVisitors
-    end
-
     class ToSql
       private
 
